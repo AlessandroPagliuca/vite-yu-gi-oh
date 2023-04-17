8061ab2a405dc1aa9769f1763fd9469ebea88bce
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-17 15:19:21 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <HeaderComponent />
+  <main>
+    
+  </main>
+</template>
+
+<script>
+  import HeaderComponent from './components/HeaderComponent.vue';
+  export default {
+    name: 'App',
+    components:{
+      HeaderComponent,
+    }
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

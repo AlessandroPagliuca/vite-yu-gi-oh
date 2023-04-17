fc58c42af5f5498ce2d2fba809849c32d17c891a
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-17 20:08:33 +0200

    Upgraded select with props

--- src/components/SrcFilterComponent.vue
+++ src/components/SrcFilterComponent.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>
-
+    
+    <div class="container p-5">
+        <select name="" id="">
+            <option value="option.archetype" selected>Alien</option>
+        </select>
     </div>
+   
 </template>
 
 <script>
     export default {
-        
+        name: 'SrcFilterComponent',
+        data(){
+            return{
+                props:['option']
+            }
+        }
     }
 </script>
 

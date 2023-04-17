cc2d3f22d18e9878c177ad9ab68636ef7d8e07ca
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-17 20:08:03 +0200

    Upgraded CounterComponent

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="col-12 bg-dark">
+        <p class="text-white fw-semibold">Found <span>{{ count }}</span> cards</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Counter',
+        data(){
+            return{
+                count: 0,
+            }
+        },
+        methods:{
+         
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
